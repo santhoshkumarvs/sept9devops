1b964d4fb987bc4f993d20802093dc00d3f41c46
santhosh <santhoshkumarvs437@gmail.com>
2019-09-10 12:56:42 +0530

    crrected the code error

--- aap.py
+++ aap.py
@@ -1,7 +1,5 @@
 print("Running python-file in Jenkins ")
-for i in range (1,40,1):
-
-print("..",+i)
-
-for j in range(40,1,-1):
+for i in range(1,40,1):
+    print("..",+i)
+for j in range(40,0,-1):
     print ("...."+j)
